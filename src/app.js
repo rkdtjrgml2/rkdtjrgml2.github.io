8849c64e09627bda296a05387a5eaf8cf1f184b5
rkdtjrgml2 <shkang2443@gmail.com>
2022-01-23 17:23:57 +0900

    seokhee v2

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
